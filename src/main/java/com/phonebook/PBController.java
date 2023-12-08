c43f3b2a26616e6b508fad0ad810b18bb4a0bb7f
unknown <szabodavid525@gmail.com>
2023-12-08 22:23:29 +0100

    change table columns to editable in PBController

--- src/main/java/com/phonebook/PBController.java
+++ src/main/java/com/phonebook/PBController.java
@@ -4,6 +4,7 @@ import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
@@ -48,19 +49,49 @@ public class PBController implements Initializable {
         lastNameCol.setMinWidth(100);
         lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
         lastNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("lastName"));
+        editCommitEvent(lastNameCol, "LastName");
         
         TableColumn firstNameCol = new TableColumn("FirstName");
         firstNameCol.setMinWidth(100);
         firstNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
         firstNameCol.setCellValueFactory(new PropertyValueFactory<Person, String>("firstName"));
+        editCommitEvent(firstNameCol, "FirstName");
         
         TableColumn emailCol = new TableColumn("EmailAddress");
         emailCol.setMinWidth(200);
         emailCol.setCellFactory(TextFieldTableCell.forTableColumn());
         emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
+        editCommitEvent(emailCol, "Email");
         
         table.getColumns().addAll(lastNameCol, firstNameCol, emailCol);
         table.setItems(data);
+    }
+    
+    private void editCommitEvent(TableColumn tableCol, String cellName) {
+        tableCol.setOnEditCommit(
+            new EventHandler<TableColumn.CellEditEvent<Person, String>> () {
+                @Override
+                public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                    getCurrentColumn(t, cellName);
+                }
+            });
+    }
+    
+    private void getCurrentColumn(TableColumn.CellEditEvent<Person, String> t, String name) {
+        switch (name) {
+            case "LastName":
+            ((Person) t.getTableView().getItems().get(
+                    t.getTablePosition().getRow())
+                            ).setLastName(t.getNewValue());
+            case "FirstName":
+            ((Person) t.getTableView().getItems().get(
+                    t.getTablePosition().getRow())
+                            ).setFirstName(t.getNewValue());
+            case "Email":
+            ((Person) t.getTableView().getItems().get(
+                    t.getTablePosition().getRow())
+                            ).setEmail(t.getNewValue());
+        }
     }
 
     
