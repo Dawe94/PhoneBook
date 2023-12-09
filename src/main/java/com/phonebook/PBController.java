bf59629b3a678883fec3f906f70c9a6bc9e5f2c4
unknown <szabodavid525@gmail.com>
2023-12-09 12:01:15 +0100

    Update menu in PBController and PBView files

--- src/main/java/com/phonebook/PBController.java
+++ src/main/java/com/phonebook/PBController.java
@@ -2,21 +2,33 @@ package com.phonebook;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TreeItem;
+import javafx.scene.control.TreeView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.control.cell.TextFieldTableCell;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 
 public class PBController implements Initializable {
+    
+    private final String MENU_CONTACTS = "Contacts";
+    private final String MENU_EXIT = "Exit";
+    private final String MENU_LIST = "List";
+    private final String MENU_EXPORTS = "Exports";
 
 //<editor-fold defaultstate="collapsed" desc="FXML Objects">
     @FXML
@@ -37,6 +49,7 @@ public class PBController implements Initializable {
             Pane exportPane;
 //</editor-fold>
     
+    // Adatbázis híján egy mesterséges Lista létrehozása
     private final ObservableList<Person> data =
             FXCollections.observableArrayList(
             new Person("Szabó", "Dávid", "szabod@citromail.com"),
@@ -45,6 +58,16 @@ public class PBController implements Initializable {
     
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        setTableData();
+        setMenuData();
+    }
+    
+    //<editor-fold defaultstate="collapsed" desc="setTableData">
+    public void setTableData() {
+        /* A nézet oszlopainak létrehozása
+        * minimális méretük beállítása
+        * A Person osztályal való összefűzése
+        * A felülírást megvalósító metódus hívása */
         TableColumn lastNameCol = new TableColumn("LastName");
         lastNameCol.setMinWidth(100);
         lastNameCol.setCellFactory(TextFieldTableCell.forTableColumn());
@@ -63,35 +86,96 @@ public class PBController implements Initializable {
         emailCol.setCellValueFactory(new PropertyValueFactory<Person, String>("email"));
         editCommitEvent(emailCol, "Email");
         
+        // Az oszlopok a táblára való felhelyezése
         table.getColumns().addAll(lastNameCol, firstNameCol, emailCol);
         table.setItems(data);
     }
     
     private void editCommitEvent(TableColumn tableCol, String cellName) {
+        // Esemény kezelő megvalósítása
         tableCol.setOnEditCommit(
-            new EventHandler<TableColumn.CellEditEvent<Person, String>> () {
-                @Override
-                public void handle(TableColumn.CellEditEvent<Person, String> t) {
-                    getCurrentColumn(t, cellName);
-                }
-            });
+                new EventHandler<TableColumn.CellEditEvent<Person, String>> () {
+                    @Override
+                    public void handle(TableColumn.CellEditEvent<Person, String> t) {
+                        // Oszlop kiválasztó metódus hívása
+                        getCurrentColumn(t, cellName);
+                    }
+                });
     }
     
     private void getCurrentColumn(TableColumn.CellEditEvent<Person, String> t, String name) {
+        // A megfelelő oszlop adott cellájának felülírása
         switch (name) {
             case "LastName":
-            ((Person) t.getTableView().getItems().get(
-                    t.getTablePosition().getRow())
-                            ).setLastName(t.getNewValue());
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())
+                        ).setLastName(t.getNewValue());
+                break;
             case "FirstName":
-            ((Person) t.getTableView().getItems().get(
-                    t.getTablePosition().getRow())
-                            ).setFirstName(t.getNewValue());
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())
+                        ).setFirstName(t.getNewValue());
+                break;
             case "Email":
-            ((Person) t.getTableView().getItems().get(
-                    t.getTablePosition().getRow())
-                            ).setEmail(t.getNewValue());
+                ((Person) t.getTableView().getItems().get(
+                        t.getTablePosition().getRow())
+                        ).setEmail(t.getNewValue());
+                break;
         }
+    }
+//</editor-fold>
+
+    private void setMenuData() {
+        // Fő menü létrehozása
+        TreeItem<String> treeItemRoot = new TreeItem<>("Menu");
+        TreeView<String> treeView = new TreeView<>(treeItemRoot);
+        
+        //A fő menü láthatóságának kikapcsolása
+        treeView.setShowRoot(false);
+        
+        // A főmenü alatti menük létrehozása (ezek már láthatóak)
+        TreeItem<String> treeItemContacts = new TreeItem<>(MENU_CONTACTS);
+        TreeItem<String> treeItemExit = new TreeItem<>(MENU_EXIT);
+        
+        // Contacts menü alapértelmezett lenyitása
+        treeItemContacts.setExpanded(true);
+        
+        // Képek Node-kénti beimportálása
+        Node contactNode = new ImageView(new Image(getClass().getResourceAsStream("contacts.png")));
+        Node exportNode = new ImageView(new Image(getClass().getResourceAsStream("export.png")));
+        
+        // Contacts menü almenüinek létrehozása és képek hozzáfűzése
+        TreeItem<String> treeItemContactsList = new TreeItem<>(MENU_LIST, contactNode);
+        TreeItem<String> treeItemContactsExports = new TreeItem<>(MENU_EXPORTS, exportNode);
+        
+        // Menü részek összefűzése
+        treeItemContacts.getChildren().addAll(treeItemContactsList, treeItemContactsExports);
+        treeItemRoot.getChildren().addAll(treeItemContacts, treeItemExit);
+        
+        // Menü rendszer felhelyezése a menuPane-re
+        menuPane.getChildren().add(treeView);
+        
+        // Egykattintásos legördítés megvalósítása
+        treeView.getSelectionModel().selectedItemProperty().addListener(new ChangeListener() {
+            @Override
+            public void changed(ObservableValue observable, Object oldValue, Object newValue) {
+                TreeItem<String> selectedItem = (TreeItem<String>) newValue;
+                String selectedMenu = selectedItem.getValue();
+                if (selectedMenu != null) {
+                    switch(selectedMenu) {
+                        case MENU_CONTACTS:
+                            try {
+                                treeItemContacts.setExpanded(true);
+                            } catch (Exception ex) {
+                            }
+                            break;
+                        case MENU_EXIT:
+                            System.exit(0);
+                            break;
+                    }
+                }
+            }
+        });
     }
 
     
